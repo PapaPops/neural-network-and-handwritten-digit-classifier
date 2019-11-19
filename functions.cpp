6b358feabbd59cf57456a706c5cb1c462ea1f7c1
pops <35334035+david-cons@users.noreply.github.com>
2019-11-19 13:07:27 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,45 +1,49 @@
 #include "functions.h"
+#include <math.h>
 
-float power(float,int);
+const float E = 2.71828;
 
 
 float Function::sigmoid(float nr)
 {
-    return nr;
+    return 1/(1+power(E,-nr));
 }
 
 float Function::tanh(float nr)
 {
-    return nr;
+    return (power(E,nr) - power(E,-nr))/
+                (power(E,nr) + power(E,-nr));
 }
 
-float Function::rrelu(float nr)
+float Function::relu(float nr)
 {
-    return nr;
+    return (nr <= 0)? 0 : nr;
 }
 
 float Function::arctan(float nr)
 {
-    return nr;
+    return (float)arctan(nr);
 }
 
 //derivatives
 
 float Function::sigmoid_d(float nr)
 {
-    return nr;
+    float result = sigmoid(nr);
+
+    result/(1-result);
 }
 float Function::tanh_d(float nr)
 {
-    return nr;
+    return 1-power(tanh(nr),2);
 }
-float Function::rrelu_d(float nr)
+float Function::relu_d(float nr)
 {
-    return nr;
+    return (nr<=0)? 0 : 1;
 }
 float Function::arctan_d(float nr)
 {
-    return nr;
+    return 1 / (power(nr,2) + 1);
 }
 
 
@@ -47,22 +51,20 @@ float Function::arctan_d(float nr)
 
 float power(float nr,int toPower)
 {
-    if(power == 0)
+    if(toPower == 0)
         return 1;
-    if(power < 0)
-    {
-        return 1/power(nr,-toPower);
-    }
-    else
-    {
-        float result = nr;
 
-        for(int i = 0;i<toPower-1;i++)
+    else if(toPower > 0)
+    {
+        int product = 1;
+        for(int i = 1; i <= toPower; i++)
         {
-            result*=nr;
+            product *= nr;
         }
 
-        return result;
+        return product;
     }
-    
+
+    else
+        return (float)1/power(nr,-toPower);
 }
